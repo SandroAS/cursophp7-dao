8578285bfbcb7144458b730c7ea0c389de2df95d
Sandro Souza <sandroantoniosouza98@gmail.com>
2021-02-06 12:32:32 -0300

    finalmente deu certo, era um espaço no lugar errado

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require_once("config.php");
 
 /*//Carrega um usuário
 $root = new Usuario();
-$root->loadbyId(3);
-echo $root;
+$root->loadbyId(2);
+echo $root;*/
 //---------------------------------------------
 //Carrega lista de usuários
 $lista = Usuario::getList();
 
-echo json_encode($lista);*/
+echo json_encode($lista);
 
-$sql = new Sql();
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
 
 ?>
