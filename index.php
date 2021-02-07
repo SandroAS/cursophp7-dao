e1dbd689889dcf2a9c0e8207e1902cbb923fd3d2
Sandro Souza <sandroantoniosouza98@gmail.com>
2021-02-07 10:03:20 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -20,9 +20,21 @@ require_once("config.php");
 //echo json_encode($search);
 //---------------------------------------------
 //Carrega um usuário usando o login e senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+//echo $usuario;
+//---------------------------------------------
+//Criando um novo usuário
+//$aluno = new Usuario("aluno", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+//---------------------------------------------
+
 $usuario = new Usuario();
-$usuario->login("root", "!@#$");
+
+$usuario->loadById(12);
+
+$usuario->update("professor", "!@#$%¨&*");
 
 echo $usuario;
-
 ?>
